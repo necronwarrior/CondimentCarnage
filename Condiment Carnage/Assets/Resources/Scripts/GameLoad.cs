870050ab5b2c73213b1809c1aa353142f1cc0e31
necronwarrior <necron_warrior@hotmail.co.uk>
2016-05-08 18:50:32 +0100

    Initial Commit Inc file structure and basic physics/UI work.

---
+++ Condiment Carnage/Assets/Resources/Scripts/GameLoad.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+
+public class GameLoad : MonoBehaviour {
+
+	int stage;// specify what stage is being used
+
+	List<PlayerIndex> playerIndex;// index list containing which players are playing
+	List<GameObject> playerUI; //UI for indivitual players
+
+	GameObject UICanvas;// the UICanvas for displaying information to the player
+
+	// Use this for initialization
+	void Start () {
+
+		UICanvas = GameObject.Find ("UI_Canvas"); //find the games UI
+
+		playerIndex = new List<PlayerIndex> (); // initilise list of players
+		playerUI = new List<GameObject>(); //initilise list of UI for each player
+
+		//determine how many players
+		for (int i = 0; i < 4; ++i)
+		{
+			//load in all images
+			playerUI.Add((GameObject)Instantiate(Resources.Load("UIPrefabs/PlayerUI/Imagetest")));//add ui for each player
+
+			playerUI[i].GetComponent<RectTransform>().SetParent(UICanvas.transform);
+			RectTransform gdygdy = playerUI[i].GetComponent<RectTransform>();
+			playerUI[i].GetComponent<Image>().enabled = false;
+
+			PlayerIndex testPlayerIndex = (PlayerIndex)i;
+			GamePadState testState = GamePad.GetState(testPlayerIndex);
+			if (testState.IsConnected)
+			{
+				playerIndex.Add(testPlayerIndex); //add connected players to the list
+				//playerUI[i].enabled = true;
+				playerUI[i].GetComponent<RectTransform>().anchoredPosition = new Vector3(UICanvas.GetComponent<RectTransform>().rect.xMax*0.8f, 
+				                                                                         UICanvas.GetComponent<RectTransform>().rect.yMax*0.8f,
+				                                                                         0.0f);
+				playerUI[i].GetComponent<Image>().enabled = true;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i<playerIndex.Count; ++i) {
+			PlayerUI((int)playerIndex[i]);	
+		}
+	}
+
+	void PlayerUI (int player) {
+		switch((player+1))
+		{
+		case 1: break;
+		case 2: break;
+		case 3: break;
+		case 4: break;
+		default: break;
+		};
+
+	}
+}
---
+++ Condiment Carnage/Assets/Resources/Scripts/NotGangBeastsMovement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class NotGangBeastsMovement : MonoBehaviour {
+	public Vector3 forwardxz;
+    float PrevInAir;
+	public Rigidbody PlayerRB;
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    bool OneJump;
+
+    // Use this for initialization
+    void Start () {
+        forwardxz = GetComponent<Rigidbody>().transform.up;
+		PlayerRB = GetComponent<Rigidbody> ();
+        OneJump = false;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+                prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        if(prevState.ThumbSticks.Left.X < -0.1){
+			PlayerRB.AddForce(Vector3.left *5.0f);
+		}
+
+		if(prevState.ThumbSticks.Left.X > 0.1){
+			PlayerRB.AddForce(Vector3.right *5.0f);
+		}
+
+		if(prevState.ThumbSticks.Left.Y > 0.1)
+        {
+			PlayerRB.AddForce(Vector3.forward *5.0f);
+		}
+
+		if(prevState.ThumbSticks.Left.Y < -0.1)
+        {
+			PlayerRB.AddForce(Vector3.back *5.0f);
+		}
+
+		if(prevState.Buttons.A == ButtonState.Released && 
+            state.Buttons.A == ButtonState.Pressed &&
+            OneJump == true)
+        {
+			PlayerRB.AddForce(Vector3.up *500.0f);
+		}
+
+        if (PrevInAir == transform.position.y)
+        {
+            OneJump = true;
+        }else
+        {
+           // if(PrevInAir != transform.position.y){
+                OneJump = false ;
+            //}
+        }
+
+
+		forwardxz.y = 0;
+		PlayerRB.transform.up = Vector3.Lerp(Vector3.up, forwardxz, Time.deltaTime);
+        PrevInAir = transform.position.y;
+    }
+}
